5b9f63e427a5b5d5f9f527780993ef8b934e6efc
NSingh <nsingh3104@gmail.com>
2017-10-03 17:50:27 -0400

    completed using codewars morseAlphabet

--- 6kyu/decode_the_morse_code.go
+++ 6kyu/decode_the_morse_code.go
@@ -8,22 +8,11 @@ import (
 // Have to findout what the map is called on codewars
 
 func DecodeMorse(morseCode string) string {
-	morseAlphabet := map[string]string {
-		"..-.": "F", "-..-": "X",
-		".--.": "P", "-": "T", "..---": "2",
-		"....-": "4", "-----": "0", "--...": "7",
-		"...-": "V", "-.-.": "C", ".": "E", ".---": "J",
-		"---": "O", "-.-": "K", "----.": "9", "..": "I",
-		".-..": "L", ".....": "5", "...--": "3", "-.--": "Y",
-		"-....": "6", ".--": "W", "....": "H", "-.": "N", ".-.": "R",
-		"-...": "B", "---..": "8", "--..": "Z", "-..": "D", "--.-": "Q",
-		"--.": "G", "--": "M", "..-": "U", ".-": "A", "...": "S", ".----": "1",
-		}
 	wordSlice := strings.Split(morseCode, " ")
 	retStr := ""
 	isPresentPrev := 0
 	for i := 0; i <  len(wordSlice); i++ {
-		mapVal, isPresent := morseAlphabet[wordSlice[i]]
+		mapVal, isPresent := MORSE_CODE[wordSlice[i]]
 		if isPresent {
 			retStr += mapVal
 		} else {
